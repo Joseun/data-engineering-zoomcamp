2c947726aa4408bb16b16d0f0e4707923c1cdbd1
Joseph Ologunja <ologanj@gmail.com>
2023-02-21 11:04:37 +0000

    dbt build running successfully

--- week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ week_4_analytics_engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -3,8 +3,8 @@
 with tripdata as 
 (
   select *,
-    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
-  from {{ source('staging','yellow_tripdata') }}
+    row_number() over(partition by CAST(vendorid AS integer), tpep_pickup_datetime) as rn
+  from {{ source('staging','yellow_taxi_trips') }}
   where vendorid is not null 
 )
 select
